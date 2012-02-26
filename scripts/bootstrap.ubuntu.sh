0fc9bc6446568eb7c827540659703a598c11902d
gabrtv <gabriel@opdemand.com>
2012-02-25 19:52:53 -0700

    Revert "move shell sourcing to instance methods"
    This reverts commit 4caacd267c2576866f48cd8954de757f03d578d6.

--- scripts/bootstrap.ubuntu.sh
+++ scripts/bootstrap.ubuntu.sh
@@ -1,3 +1,19 @@
+
+# write out inputs for shell sourcing
+python -c '
+#!/usr/bin/env python
+import sys
+import json
+with open("/var/cache/opdemand/inputs.json") as f:
+  data = f.read()
+  inputs = json.loads(data)
+# write out env vars
+with open("/var/cache/opdemand/inputs.sh", "w") as f:
+  for k, v in iter(inputs.items()):
+    key = k.replace("/", "_").lower()
+    f.write("%s=\"%s\"\n" % (key, v))
+'
+
 # link all scripts to public path
 for script in `ls /var/lib/opdemand/bin`; do
   ln -fs /var/lib/opdemand/bin/$script /usr/local/bin/$script

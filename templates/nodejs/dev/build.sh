6a7d1217d0b9a6497b036fcee01bc7f7aa78e47c
gabrtv <gabriel@opdemand.com>
2012-03-02 20:01:32 -0700

    remove symlinks

--- templates/nodejs/dev/build.sh
+++ templates/nodejs/dev/build.sh
@@ -1,1 +1,41 @@
-../../../scripts/build.puppet.sh
+#!/bin/sh
+
+# OpDemand build script for Puppet-managed servers
+# This script runs as "root" on first boot
+# typically invoked by OpDemand orchestration
+
+# set locale for calls that require encoding
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# prevent dpkg prompting
+export DEBIAN_FRONTEND=noninteractive
+
+#
+# puppet-specific bootstrapping
+#
+
+# install puppet packages and gems
+apt-get install puppet rubygems -yq --force-yes
+gem install puppet-module hiera hiera-json hiera-puppet
+
+# install hiera config file
+cat > /etc/puppet/hiera.yaml <<EOF
+---
+:backends: - json
+           - puppet
+
+:logger: console
+
+:hierarchy: - inputs
+
+:json:
+   :datadir: /var/cache/opdemand
+
+:puppet:
+   :datasource: data
+EOF
+
+# clone opdemand puppet modules to a puppet-path'd directory
+mkdir -p /usr/share/puppet
+git clone --recursive git://github.com/opdemand/puppet-modules /usr/share/puppet/modules

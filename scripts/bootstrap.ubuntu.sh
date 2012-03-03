fdb7feae7771d658ed764fbe67b88146f569b58c
gabrtv <gabriel@opdemand.com>
2012-03-02 20:01:25 -0700

    remove unused bootstrap/deploy scripts

--- scripts/bootstrap.ubuntu.sh
+++
@@ -1,7 +0,0 @@
-
-# link all scripts to public path
-for script in `ls /var/lib/opdemand/bin`; do
-  ln -fs /var/lib/opdemand/bin/$script /usr/local/bin/$script
-done
-
-# /usr/local/bin/opdemand-build will be triggered

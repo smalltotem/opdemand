addce67e14367287d92b742e92eb69a7ccef0185
gabrtv <gabriel@opdemand.com>
2012-03-15 11:10:26 -0600

    add debconf preseeding to postfix install

--- templates/postfix/elbx2/deploy.sh
+++ templates/postfix/elbx2/deploy.sh
@@ -10,5 +10,17 @@ else
     debug_flag="-v"
 fi
 
-# execute puppet apply
+# lookup hostnames
+public_hostname=`curl http://169.254.169.254/latest/meta-data/public-hostname`
+local_hostname=`curl http://169.254.169.254/latest/meta-data/local-hostname`
+
+# install required packages
+apt-get install -yq --force-yes debconf-utils
+
+# preseed debconf for postfix
+echo "postfix postfix/main_mailer_type select Internet Site
+postfix postfix/mailname string $public_hostname
+postfix postfix/destination string $public_hostname, $local_hostname, localhost.ec2.internal, localhost" | debconf-set-selections
+
+# install postfix
 apt-get install postfix -yq --force-yes

695799bf1771fcdc2953fdfa55633477dee83492
gabrtv <gabriel@opdemand.com>
2012-04-08 20:58:18 -0600

    first pass at rails 1x1

---
+++ templates/rails/postgresql/1x1/deploy.app.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+# source opdemand common and env vars
+. /var/cache/opdemand/inputs.sh
+
+# check for debug flag
+if [ $server_debug = "True" ] ; then
+    debug_flag="-d"
+else
+    debug_flag="-v"
+fi
+
+# execute puppet apply
+puppet apply $debug_flag -e 'include opdemand::framework::rails::postgresql'
